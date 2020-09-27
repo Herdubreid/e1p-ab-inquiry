cb80f5a835682255f6cacd0efb0e4059fcf9a869
Finnur Bragason <fbragason@outlook.com>
2020-09-27 11:04:48 +0800

    update packages

--- webpack.common.js
+++ webpack.common.js
@@ -25,53 +25,58 @@ module.exports = {
             }
         }),
         new webpack.ProvidePlugin({
-            jquery: 'jquery',
-            Popper: ['popper.js', 'default']
+            $: 'jquery',
+            jQuery: 'jquery'
         }),
-        new ForkTsCheckerWebpackPlugin()
+        new ForkTsCheckerWebpackPlugin(),
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb/)
     ],
     module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
+        rules: [{
+            test: /\.(ts|js)x?$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        },
+        {
+            test: /\.(css)$/,
+            use: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.(scss)$/,
+            use: [{
+                // Adds CSS to the DOM by injecting a `<style>` tag
+                loader: 'style-loader'
             },
             {
-                test: /\.(scss)$/,
-                use: [
-                    {
-                        // Adds CSS to the DOM by injecting a `<style>` tag
-                        loader: 'style-loader'
-                    },
-                    {
-                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
-                        loader: 'css-loader'
-                    },
-                    {
-                        // Loader for webpack to process CSS with PostCSS
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: function() {
-                                return [require('autoprefixer')];
-                            }
-                        }
-                    },
-                    {
-                        // Loads a SASS/SCSS file and compiles it to CSS
-                        loader: 'sass-loader'
-                    }
-                ]
+                // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                loader: 'css-loader',
             },
             {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                        options: { minimize: true }
-                    }
-                ]
+                // Loader for webpack to process CSS with PostCSS
+                loader: 'postcss-loader'
+            },
+            {
+                // Loads a SASS/SCSS file and compiles it to CSS
+                loader: 'sass-loader'
             }
+            ]
+        },
+        {
+            test: /\.(png|jpg|gif)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {},
+            },],
+        },
+        {
+            test: /\.html$/,
+            use: [{
+                loader: 'html-loader',
+                options: {
+                    minimize: true
+                }
+            }]
+        }
         ]
     },
     output: {
